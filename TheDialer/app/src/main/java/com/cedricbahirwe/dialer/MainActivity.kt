7dff021c8cd0f7e6c6942b185ecc6e47cfc5826b
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2023-01-21 21:49:19 +0200

    Fix Kotlin compiler

--- TheDialer/app/src/main/java/com/cedricbahirwe/dialer/MainActivity.kt
+++ TheDialer/app/src/main/java/com/cedricbahirwe/dialer/MainActivity.kt
@@ -6,16 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AddCircle
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.cedricbahirwe.dialer.ui.theme.DialerTheme
+import com.cedricbahirwe.dialer.ui.theme.MainRed
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,6 +60,18 @@ fun DashBoardContainer() {
             .fillMaxWidth()
             .padding(10.dp)
     ) {
+
+        Row {
+            Text(
+                "Dialer",
+                modifier = Modifier.padding(vertical = 15.dp),
+                fontSize = 30.sp,
+                fontWeight = FontWeight.ExtraBold
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+        }
+
         val itemModifier = Modifier
             .fillMaxWidth()
             .height(80.dp)
@@ -75,6 +91,36 @@ fun DashBoardContainer() {
             DashBoardItem("History", modifier = itemModifier)
             Spacer(modifier = Modifier.width(16.dp))
             DashBoardItem("My Space", modifier = itemModifier)
+        }
+
+        Spacer(modifier = Modifier.weight(1.0f))
+
+        Row(modifier = Modifier.padding(vertical = 8.dp)) {
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = MainRed,
+                    contentColor = Color.White
+                ),
+            ) {
+                Icon(
+                    Icons.Rounded.AddCircle,
+                    contentDescription = "Quick Dial icon"
+                )
+                Text("Add to cart", Modifier.padding(start = 10.dp))
+            }
+            Spacer(modifier = Modifier.weight(1.0f))
+
+            Row(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(Color.White)
+                    .padding(7.dp)
+            ) {
+                Text("|||")
+                Spacer(modifier = Modifier.width(8.dp))
+                Text("MTN")
+            }
         }
     }
 }
