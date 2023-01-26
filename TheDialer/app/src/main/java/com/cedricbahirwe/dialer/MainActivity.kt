d2beda57b304976240c7d4076891ecfce8d562cb
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2023-01-26 22:42:21 +0200

    Nice

--- TheDialer/app/src/main/java/com/cedricbahirwe/dialer/MainActivity.kt
+++ TheDialer/app/src/main/java/com/cedricbahirwe/dialer/MainActivity.kt
@@ -3,13 +3,17 @@ package com.cedricbahirwe.dialer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cedricbahirwe.dialer.ui.theme.DialerTheme
@@ -21,23 +25,50 @@ class MainActivity : ComponentActivity() {
             DialerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    DiceRollerApp()
                 }
             }
         }
     }
 }
-@Composable
-fun Greeting(name: String) {
-    Surface(color = Color.Magenta) {
-        Text(text = "Hi, my name is $name!", modifier = Modifier.padding(24.dp))
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    DialerTheme {
-        Greeting("Cedric")
+fun DiceRollerApp() {
+    DiceWithButtonAndImage(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    )
+}
+
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result by remember { mutableStateOf(1) }
+    val imageResource = when(result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
     }
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = imageResource),
+            contentDescription = "Dice at $result"
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text("Something $result")
+        Button(onClick = { result = (1..6).random() }) {
+            Text(stringResource(R.string.roll))
+        }
+    }
+
+
 }
