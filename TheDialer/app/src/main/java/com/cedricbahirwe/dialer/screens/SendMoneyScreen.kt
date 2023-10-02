7d22529d0edf808736a5e96436e8dec08ac6f7da
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2023-10-02 13:25:19 +0200

    complete minimal navigation

--- TheDialer/app/src/main/java/com/cedricbahirwe/dialer/screens/SendMoneyScreen.kt
+++ TheDialer/app/src/main/java/com/cedricbahirwe/dialer/screens/SendMoneyScreen.kt
@@ -1,13 +1,11 @@
 package com.cedricbahirwe.dialer.screens
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -28,7 +26,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
