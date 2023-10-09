02615fadcf6a1b7556e63c5be054baaf7f91b630
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2023-10-09 14:13:24 +0200

    Minor fix

--- TheDialer/app/src/main/java/com/cedricbahirwe/dialer/screens/SettingsScreen.kt
+++ TheDialer/app/src/main/java/com/cedricbahirwe/dialer/screens/SettingsScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -23,7 +22,6 @@ import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Switch
-import androidx.compose.material.SwitchColors
 import androidx.compose.material.SwitchDefaults
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
