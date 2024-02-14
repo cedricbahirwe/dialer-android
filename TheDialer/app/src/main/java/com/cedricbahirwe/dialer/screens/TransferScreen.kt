9b2e32ce62406d5e07b16b84a910ce617ab01d84
Esther Carrelle <esthcarelle@gmail.com>
2024-02-14 18:19:17 +0200

    Update phone number field after selecting a number

--- TheDialer/app/src/main/java/com/cedricbahirwe/dialer/screens/TransferScreen.kt
+++ TheDialer/app/src/main/java/com/cedricbahirwe/dialer/screens/TransferScreen.kt
@@ -1,5 +1,13 @@
 package com.cedricbahirwe.dialer.screens
 
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.provider.ContactsContract
+import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -42,6 +50,9 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.cedricbahirwe.dialer.R
 import com.cedricbahirwe.dialer.common.TitleView
@@ -58,17 +69,25 @@ fun TransferView(
         factory = TransferViewModelFactory(
             LocalContext.current
         )
-    )
+    ),
+    contactName: String,
+    contactNumber: String
 ) {
     val focusManager = LocalFocusManager.current
     val focusRequester = remember { FocusRequester() }
     val keyboardController = LocalSoftwareKeyboardController.current
+    val activity = LocalContext.current as Activity
+    val context = LocalContext.current
 
     val uiState by viewModel.uiState.collectAsState()
 
     val isMerchantTransfer = remember(uiState.isMerchantTransfer) {
         uiState.isMerchantTransfer
     }
+
+    viewModel.handleTransactionNumberChange(contactNumber)
+
+    Log.e(TAG, "TransferView: "+contactNumber )
 
     val pageTitle = if (isMerchantTransfer) {
         stringResource(id = R.string.pay_merchant)
@@ -144,7 +163,9 @@ fun TransferView(
                     onValueChange = {
                         viewModel.handleTransactionAmountChange(it)
                     },
-                    modifier = Modifier.fillMaxWidth().focusRequester(focusRequester),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .focusRequester(focusRequester),
                     label = {
                         Text(stringResource(R.string.common_amount))
                     },
@@ -218,7 +239,18 @@ fun TransferView(
                     visible = !isMerchantTransfer
                 ) {
                     Button(
-                        onClick = {},
+                        onClick = {
+                            // on below line checking if permission is granted.
+                            if (hasContactPermission(context)) {
+                                // if permission granted open intent to pick contact/
+                                val intent = Intent(Intent.ACTION_GET_CONTENT)
+                                intent.type = ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE
+                                startActivityForResult(activity, intent, 1, null)
+                            } else {
+                                // if permission not granted requesting permission .
+                                requestContactPermission(context, activity)
+                            }
+                        },
                         Modifier
                             .fillMaxWidth()
                             .height(48.dp),
@@ -266,5 +298,22 @@ fun TransferView(
             Spacer(modifier = Modifier.weight(1.0f))
         }
     }
+
 }
+
+// Function to open the contact picker and handle the result
+fun hasContactPermission(context: Context): Boolean {
+    // on below line checking if permission is present or not.
+    return ContextCompat.checkSelfPermission(context, Manifest.permission.READ_CONTACTS) ==
+            PackageManager.PERMISSION_GRANTED;
+}
+
+fun requestContactPermission(context: Context, activity: Activity) {
+    // on below line if permission is not granted requesting permissions.
+    if (!hasContactPermission(context)) {
+        ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.READ_CONTACTS), 1)
+    }
+}
+
+
 
